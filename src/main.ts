38329eaa33d457567b432db641105018b51a60a6
marades <jkl9510357@gmail.com>
2023-06-03 18:08:00 +0900

    Feat: Response Intercepter Setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './core/response.intercepter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
+
   await app.listen(3000);
 }
 bootstrap();
