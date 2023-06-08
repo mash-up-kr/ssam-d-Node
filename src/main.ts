cc5f336b00afc3a0194781cfe49128c82ffee22b
marades <jkl9510357@gmail.com>
2023-06-08 14:02:48 +0900

    Chore: intercepter 폴더 분리

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ResponseInterceptor } from './core/response.intercepter';
+import { ResponseInterceptor } from './core/intercepters/response.intercepter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalInterceptors(new ResponseInterceptor())
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
 }
