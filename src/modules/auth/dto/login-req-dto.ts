08924bb8e02a1e01d5be2f54bd14c6c10424db89
hye-on <ain0103@naver.com>
2023-06-10 16:55:12 +0900

    docs : KAKAO 로그인 API 문서 작성

--- src/modules/auth/dto/login-req-dto.ts
+++ src/modules/auth/dto/login-req-dto.ts
@@ -4,10 +4,37 @@ import { IsEmail } from 'class-validator';
 
 //회원가입
 export class LoginReqDto {
+  /**
+   * 닉네임
+   *
+   * @type {string}
+   * @example 혜온
+   *
+   */
   nickname: string;
+
+  /**
+   * 이메일
+   *
+   * @type {string}
+   * @example ain0103@naver.com
+   */
   @IsEmail()
   email: string;
+
+  /**
+   *카카오 회원 아이디
+   * @type {string}
+   * @example 0342
+   */
   socialId: string;
+
+  /**
+   *
+   *소셜로그인 종류
+   * @type {string}
+   * @example KAKAO
+   */
   provider: string;
 }
 
