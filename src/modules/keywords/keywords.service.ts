8790d6e2e1003309dc143868ebe70600f5a9bde3
marades <jkl9510357@gmail.com>
2023-06-14 02:06:56 +0900

    Feat: keyword 추출 api

--- src/modules/keywords/keywords.service.ts
+++ src/modules/keywords/keywords.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Keyword } from 'src/domains/keyword';
 import { KeywordRepository } from 'src/repositories';
+import * as mecab from 'mecab-ya';
 
 @Injectable()
 export class KeywordsService {
@@ -15,4 +16,31 @@ export class KeywordsService {
     const keywords = await this.keywordRepository.getList(plainKeywords);
     return keywords;
   }
+
+  async recommend(content: string) {
+    const keywordDic = await this.extract(content);
+    return Object.entries(keywordDic)
+      .sort((a, b) => b[1] - a[1])
+      .map(([keyword]) => keyword);
+  }
+
+  private async extract(content: string): Promise<KeywordDic> {
+    return new Promise((resolve, reject) => {
+      mecab.pos(content, (err: unknown, result: MecabOutput) => {
+        if (err) reject(err);
+        const keywords = result.filter(([_, form]) => form === 'NNG').map(([word]) => word);
+        const keywordDic = keywords.reduce((dic, keyword) => {
+          if (!dic[keyword]) dic[keyword] = 0;
+          dic[keyword]++;
+          return dic;
+        }, {});
+        resolve(keywordDic);
+      });
+    });
+  }
 }
+
+type MecabOutput = string[][];
+type KeywordDic = {
+  [keyword: string]: number;
+};
