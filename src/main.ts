7eba4c575acd10779966875bf97394ea1a47e204
marades <jkl9510357@gmail.com>
2023-06-15 00:27:35 +0900

    Chore: port env 분리

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,21 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './core/intercepters/response.intercepter';
 import { InitApiDocMiddleware } from './docs';
 import { RequestMethod } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
 
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.setGlobalPrefix('api', {
     exclude: [{ path: 'docs', method: RequestMethod.GET }],
   });
 
-  await InitApiDocMiddleware(app);
+  await InitApiDocMiddleware(app, port);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
