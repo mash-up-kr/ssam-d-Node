f3cd59ff7676ebe171ecc9cf495f4545684ae1c3
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-16 01:42:18 +0900

    feat: new Hot Reload 추가
    - env 파일 main.ts 에서 받도록 수정

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { InitApiDocMiddleware } from './docs';
 import { RequestMethod } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CustomExceptionFilter } from './core/exception-filters/custom-exception.filter';
+import * as dotenv from 'dotenv';
+import { IS_DEV } from './common/constants';
+
+declare const module: any;
 
 async function bootstrap() {
+  if (IS_DEV) {
+    dotenv.config({ path: '.env.development' });
+  }
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
