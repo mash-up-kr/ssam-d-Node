b7e6d0e8bbeaf20c36b3bc99c589e89d75ecbc58
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-17 00:19:26 +0900

    chore: dev 환경분리

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,3 @@
-export const IS_DEV = process.env.NODE_ENV === 'development' || false;
+export const IS_LOCAL = process.env.NODE_ENV === 'local';
+export const IS_DEV = process.env.NODE_ENV === 'development';
+export const IS_PROD = process.env.NODE_ENV === 'production';
