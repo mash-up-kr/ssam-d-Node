459d652deba7558241c3363151510a99ef77e4f5
hye-on <ain0103@naver.com>
2023-06-22 16:57:14 +0900

    feat : signal 컨트롤러,서비스,리포지토리 생성

--- src/modules/signal/signal.controller.ts
+++ src/modules/signal/signal.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { SignalService } from './signal.service';
-
+import { SignalReqDto } from './dto/signal-req-dto';
 @Controller('signal')
 export class SignalController {
   constructor(private readonly signalService: SignalService) {}
+  /**
+   * 키워드로 일치하는 사람 고르기
+   * 매칭
+   * 없으면 트래쉬
+   * 있으면 시그널
+   */
+  @Post('/send')
+  async recommend(@Body() signalReqDto: SignalReqDto) {}
 }
