f61b0fb93d7d8a672b46ad2fc86bf12af5986869
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-23 12:57:53 +0900

    test: user delete api test 추가

---
+++ test/mock/data/user.data.mock.ts
@@ -0,0 +1,12 @@
+import { User } from 'src/domains/user';
+
+export const userDataObject = [
+  {
+    id: 1,
+    nickname: 'testNickName',
+    email: 'test@test.com',
+    socialId: 'testSocialId',
+    provider: 'testProvider',
+    agreeAlarm: true,
+  },
+];
