1f9fcdb73ce6d5243e5910e33ea127036ea38ac0
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-23 11:49:49 +0900

    feat: user 삭제 api 추가

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthUser } from 'src/common/decorators/auth-user.decorator';
 import { AuthGuard } from '../auth/guards/jwt.auth.guard';
@@ -11,6 +11,11 @@ export class UsersController {
   @Get('/nickname/duplication')
   async isDuplicatedNickname(@Query('nickname') nickname: string) {
     await this.usersService.isDuplicatedNickname(nickname);
+  }
+
+  @Delete('/:id')
+  async deleteUserById(@Param('id') id: string) {
+    await this.usersService.deleteById(+id);
   }
 
   @UseGuards(AuthGuard)
