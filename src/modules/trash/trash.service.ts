51f21e2eb98a02eb9e5cb1cede1130d835a5c3e4
hye-on <ain0103@naver.com>
2023-06-25 21:19:06 +0900

    Chore : 안쓰는 import 삭제

--- src/modules/trash/trash.service.ts
+++ src/modules/trash/trash.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { TrashRepository } from 'src/repositories';
-import { SignalReqDto } from '../signal/dto/signal-req-dto';
 
 @Injectable()
 export class TrashService {
