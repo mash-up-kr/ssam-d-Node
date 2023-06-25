9b4bad4750b8560f89a41f276fb93691df49e51b
hye-on <ain0103@naver.com>
2023-06-25 17:24:19 +0900

    feat : 키워드, 시그널 저장

--- src/repositories/trash.repository.ts
+++ src/repositories/trash.repository.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { Trash } from 'src/domains/trash';
 @Injectable()
 export class TrashRepository {
   constructor(private readonly prisma: PrismaService) {}
+  async save(trashData: Partial<Trash>): Promise<void> {
+    await this.prisma.trash.create({
+      data: {
+        userId: trashData.userId,
+        content: trashData.content,
+        keywords: trashData.keywords,
+      },
+    });
+  }
 }
