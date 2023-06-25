3ff1953430f0a4069c3b49de882d12690540ef23
hye-on <ain0103@naver.com>
2023-06-25 17:24:19 +0900

    feat : 키워드, 시그널 저장

--- src/repositories/signal.repository.ts
+++ src/repositories/signal.repository.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Signal } from 'src/domains/signal';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SignalRepository {
   constructor(private readonly prisma: PrismaService) {}
+  async save(signal: Exclude<Signal, 'id'>[]): Promise<void> {
+    await this.prisma.signal.createMany({
+      data: signal,
+      skipDuplicates: true,
+    });
+  }
 }
