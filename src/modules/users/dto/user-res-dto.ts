f7842f84d6268e8045dbbe2bc17f67e2cc0854ef
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-25 16:42:17 +0900

    refactor: return user 정보에서 socialId 제외

--- src/modules/users/dto/user-res-dto.ts
+++ src/modules/users/dto/user-res-dto.ts
@@ -28,13 +28,6 @@ export class UserResDto {
    *
    * @readonly
    * @type {string}
-   * @example 2
-   */
-  readonly socialId: string;
-  /**
-   *
-   * @readonly
-   * @type {string}
    * @example KAKAO
    */
   readonly provider: string;
@@ -54,11 +47,10 @@ export class UserResDto {
    */
   readonly agreeAlarm: boolean;
 
-  constructor({ id, nickname, email, socialId, provider, profileImageUrl, agreeAlarm }: Partial<User>) {
+  constructor({ id, nickname, email, provider, profileImageUrl, agreeAlarm }: Partial<User>) {
     this.id = id;
     this.nickname = nickname;
     this.email = email;
-    this.socialId = socialId;
     this.provider = provider;
     this.profileImageUrl = profileImageUrl;
     this.agreeAlarm = agreeAlarm;
