34d8dd7bb5054903a14fd91202927cbc93200b6f
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-27 00:22:11 +0900

    feat: replyFirstSignal API 추가

--- src/modules/signal/signal.controller.ts
+++ src/modules/signal/signal.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Param } from '@nestjs/common';
 import { SignalService } from './signal.service';
 import { KeywordsService } from '../keywords/keywords.service';
 import { SignalReqDto } from './dto/signal-req-dto';
 import { AuthGuard } from '../auth/guards/jwt.auth.guard';
 import { AuthUser } from 'src/common/decorators/auth-user.decorator';
+
 @Controller('signal')
+@UseGuards(AuthGuard)
 export class SignalController {
   constructor(private readonly signalService: SignalService, private readonly keywordService: KeywordsService) {}
 
-  @UseGuards(AuthGuard)
   @Post('/send')
   async sendSignal(@AuthUser() senderId, @Body() signalReqDto: SignalReqDto) {
     await this.signalService.sendSignal(senderId, signalReqDto);
+  }
+
+  @Post('/:id/reply')
+  async replyFirstSignal(
+    @AuthUser() senderId,
+    @Param('id') signalId: string,
+    @Body() signalReqDto: Pick<SignalReqDto, 'content'>
+  ) {
+    await this.signalService.replyFirstSignal(+signalId, senderId, signalReqDto);
   }
 }
