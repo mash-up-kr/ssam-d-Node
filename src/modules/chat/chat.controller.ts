e85e2c3fa72af383c92d51f48665f7d616a70a13
ChanWoongLee <dlcksdnd6785@naver.com>
2023-06-28 23:49:38 +0900

    feat: chat list 조회 API 추가

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthUser } from 'src/common/decorators/auth-user.decorator';
 import { AuthGuard } from '../auth/guards/jwt.auth.guard';
 import { ChatService } from './chat.service';
 
 @UseGuards(AuthGuard)
-@Controller('chat')
+@Controller('chats')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 }
--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ChatRepository } from 'src/repositories';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly chatRepository: ChatRepository) {}
+}
