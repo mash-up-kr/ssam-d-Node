f699d4c0d7228926ec1f625bc4003e972b4a9ccf
marades <jkl9510357@gmail.com>
2023-07-03 17:54:07 +0900

    Feat: chat domain에서 private 제거 및 chat 보내기 로직에 적용

--- src/repositories/chat.repository.ts
+++ src/repositories/chat.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { Chat } from 'src/domains/chat';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaTransaction } from 'src/types/prisma.type';
 
@@ -7,9 +8,10 @@ import { PrismaTransaction } from 'src/types/prisma.type';
 export class ChatRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async save(chatData: Prisma.ChatUncheckedCreateInput, transaction?: PrismaTransaction): Promise<void> {
+  async save(chat: Chat, transaction?: PrismaTransaction): Promise<void> {
     const prisma = transaction ?? this.prisma;
-    await prisma.chat.create({ data: chatData });
+
+    await prisma.chat.create({ data: chat });
   }
 
   async saveAll(chatData: Prisma.ChatUncheckedCreateInput[], transaction?: PrismaTransaction): Promise<void> {
