a088a72deba56d50d717acb24be9703bec75a268
marades <jkl9510357@gmail.com>
2023-07-04 17:19:09 +0900

    Test: room에서 chat 보내기 테스트 자것ㅇ

--- src/modules/room/room.service.spec.ts
+++ src/modules/room/room.service.spec.ts
@@ -4,6 +4,8 @@ import { ChatRepository, RoomRepository, RoomUserRepository, UserRepository } fr
 import { MockChatRepository, MockUserRepository } from '../../../test/mock/repositories';
 import { MockRoomRepository } from '../../../test/mock/repositories/mock-room.repository';
 import { MockRoomUserRepository } from '../../../test/mock/repositories/mock-room-user.repository';
+import { RoomUser } from 'src/domains/room-user';
+import { CannotSendChatException } from 'src/exceptions';
 
 describe('RoomService', () => {
   let roomService: RoomService;
@@ -36,5 +38,26 @@ describe('RoomService', () => {
     expect(roomUserRepository).toBeDefined();
     expect(userRepository).toBeDefined();
     expect(chatRepository).toBeDefined();
+  });
+
+  describe('채팅방에서 채팅 보내기', () => {
+    const senderId = 1;
+    const roomId = 1;
+    const content = 'content';
+    it('성공', async () => {
+      roomUserRepository.get.mockResolvedValue(new RoomUser({ userId: senderId, roomId }));
+
+      const result = await roomService.sendChat(senderId, roomId, content);
+      expect(result).toBeUndefined();
+    });
+
+    it('Room과 Sender가 일치하지 않을 때', async () => {
+      roomUserRepository.get.mockResolvedValue(null);
+      try {
+        await roomService.sendChat(senderId, roomId, content);
+      } catch (e) {
+        expect(e).toBeInstanceOf(CannotSendChatException);
+      }
+    });
   });
 });
