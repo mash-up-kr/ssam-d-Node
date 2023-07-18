41fe681541f7054f62c94acf07782998e7cb2740
ChanWoongLee <dlcksdnd6785@naver.com>
2023-07-18 20:34:12 +0900

    test: sendChat test 수정

--- test/mock/repositories/mock-room-user.repository.ts
+++ test/mock/repositories/mock-room-user.repository.ts
@@ -3,4 +3,6 @@ import { MockRepository } from './mock.repository';
 
 export const MockRoomUserRepository = (): MockRepository<RoomUserRepository> => ({
   get: jest.fn(),
+  getMatchingUser: jest.fn(),
+  updateIsChatRead: jest.fn(),
 });
