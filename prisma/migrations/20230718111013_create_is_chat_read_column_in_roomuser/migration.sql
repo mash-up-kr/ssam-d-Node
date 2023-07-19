68551eea89fd12a13416ea0127da8c4bd1678ce8
ChanWoongLee <dlcksdnd6785@naver.com>
2023-07-19 18:44:34 +0900

    fix: migrate sql 수정

--- prisma/migrations/20230718111013_create_is_chat_read_column_in_roomuser/migration.sql
+++ prisma/migrations/20230718111013_create_is_chat_read_column_in_roomuser/migration.sql
@@ -2,4 +2,4 @@
 ALTER TABLE `room_user` ADD COLUMN `is_chat_read` BOOLEAN NOT NULL DEFAULT false;
 
 -- CreateIndex
-CREATE UNIQUE INDEX `room_user_user_id_room_id_key` ON `room_user`(`user_id`, `room_id`);
+-- CREATE UNIQUE INDEX `room_user_user_id_room_id_key` ON `room_user`(`user_id`, `room_id`);
