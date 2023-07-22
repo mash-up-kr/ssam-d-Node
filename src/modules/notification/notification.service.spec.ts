4818f85be33c693df5c1f938969315731ea43177
hye-on <ain0103@naver.com>
2023-07-22 16:04:32 +0900

    Chore : fcm test skip

--- src/modules/notification/notification.service.spec.ts
+++ src/modules/notification/notification.service.spec.ts
@@ -1,20 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationService } from './notification.service';
-import { ConfigService } from '@nestjs/config';
-import { fcmMockData } from 'test/mock/data/fcm.data.mock';
-describe('NotificationService', () => {
+xdescribe('NotificationService', () => {
   let service: NotificationService;
-  let configService: ConfigService;
   beforeEach(async () => {
-    const configServiceMock = {
-      get: jest.fn().mockReturnValue(fcmMockData),
-    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationService, { provide: ConfigService, useValue: configServiceMock }],
+      providers: [],
     }).compile();
 
     service = module.get<NotificationService>(NotificationService);
-    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
