4db1215a81581aa9b2d4854fa60d4ac096120b91
marades <jkl9510357@gmail.com>
2023-07-23 16:13:49 +0900

    Fix: 시그널 끊으면 다른 쪽에서도 없어지는 이슈 해결
    - prisma soft delete middleware 수정
      - findMany 추가
      - deletedAt 파라미터 순서 변경
    - room 퇴장 시 roomUser 삭제 처리 및 사이드 이펙트 반영
    - room 퇴장 시 모든 room 삭제 조건 변경 : isAlive===false -> roomUser가 모두 나갔을 때로 변경

--- src/domains/room-user.ts
+++ src/domains/room-user.ts
@@ -2,10 +2,12 @@ export class RoomUser {
   readonly id: number;
   readonly userId: number;
   readonly roomId: number;
+  readonly deletedAt: Date;
 
   constructor(roomUser: Partial<RoomUser>) {
     this.id = roomUser.id;
     this.userId = roomUser.userId;
     this.roomId = roomUser.roomId;
+    this.deletedAt = roomUser.deletedAt;
   }
 }
