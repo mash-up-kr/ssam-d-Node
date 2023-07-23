ab2b2a4efc9220b7ef4335eaecdc17223f9e49b2
ChanWoongLee <dlcksdnd6785@naver.com>
2023-07-23 22:00:39 +0900

    chore: migration 파일 수정

--- prisma/migrations/20230723114820_add_latest_chat_time_in_room/migration.sql
+++ prisma/migrations/20230723114820_add_latest_chat_time_in_room/migration.sql
@@ -4,14 +4,6 @@
   - A unique constraint covering the columns `[device_token]` on the table `device_token` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX `signal_room_id_fkey` ON `signal`;
 
 -- AlterTable
 ALTER TABLE `room` ADD COLUMN `latest_chat_time` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
--- CreateIndex
-CREATE UNIQUE INDEX `device_token_device_token_key` ON `device_token`(`device_token`);
-
--- RenameIndex
-ALTER TABLE `crash` RENAME INDEX `crash_user_id_fkey` TO `trash_user_id_fkey`;
