b3800fe3b8555fdca3c2d16cdf86bae561f7a368
hye-on <ain0103@naver.com>
2023-07-29 15:42:32 +0900

    refactor : 메세지 보내는 부분 함수로 빼기

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -2,12 +2,20 @@ import { Injectable } from '@nestjs/common';
 import * as firebaseAdmin from 'firebase-admin';
 import { DeviceTokenNotFoundException } from 'src/exceptions';
 import { ConfigService } from '@nestjs/config';
+import { BatchResponse } from 'firebase-admin/lib/messaging/messaging-api';
+
 export interface ISendFirebaseMessages {
   token: string;
   title?: string;
   message: string;
 }
 
+/**
+ * 시그널 도착했을때
+ * 챗 도착했을 때
+ * 서버에서 알림 줄 때
+ *
+ */
 @Injectable()
 export class NotificationService {
   constructor(private readonly configService: ConfigService) {
@@ -39,53 +47,62 @@ export class NotificationService {
     for (let i = 0; i < totalDeviceCount; i += MAX_BATCH_SIZE) {
       const batchDeviceIds = deviceTokenIds.slice(i, i + MAX_BATCH_SIZE);
 
-      const body: firebaseAdmin.messaging.MulticastMessage = {
-        tokens: batchDeviceIds,
-        data: payload?.data,
-        notification: {
-          title: payload?.notification?.title,
-          body: payload?.notification?.body,
-          imageUrl,
-        },
+      const result = await this.send(batchDeviceIds, payload, imageUrl);
 
-        android: {
-          priority: 'high',
-          ttl: 60 * 60 * 24,
-          notification: {
-            sound: payload?.notification?.sound,
-          },
-        },
-      };
-
-      try {
-        const result = await firebaseAdmin.messaging().sendMulticast(body, false);
-        if (result.failureCount > 0) {
-          const failedTokens = [];
-          result.responses.forEach((resp, id) => {
-            if (!resp.success) {
-              failedTokens.push(batchDeviceIds[id]);
-            }
-          });
-          failedDeviceIds.push(...failedTokens);
-        }
-
-        failureCount += result.failureCount;
-        successCount += result.successCount;
-
-        /**
-         * 클라이언트 구현 전까지 임시로 보기 위해서
-         *
-         */
-        console.log('failureCount' + failureCount);
-        console.log('successCount' + successCount);
-      } catch (error) {
-        /**
-         * Todo: Logger ,error
-         */
-        throw error;
+      if (result.failureCount > 0) {
+        const failedTokens = [];
+        result.responses.forEach((resp, id) => {
+          if (!resp.success) {
+            failedTokens.push(batchDeviceIds[id]);
+          }
+        });
+        failedDeviceIds.push(...failedTokens);
       }
+
+      failureCount += result.failureCount;
+      successCount += result.successCount;
+
+      /**
+       * 클라이언트 구현 전까지 임시로 보기 위해서
+       *
+       */
+      console.log('failureCount' + failureCount);
+      console.log('successCount' + successCount);
     }
 
     return { failureCount, successCount, failedDeviceIds };
+  }
+  private async send(
+    batchDeviceIds: string[],
+    payload: firebaseAdmin.messaging.MessagingPayload,
+    imageUrl?: string
+  ): Promise<BatchResponse> {
+    const body: firebaseAdmin.messaging.MulticastMessage = {
+      tokens: batchDeviceIds,
+      data: payload?.data,
+      notification: {
+        title: payload?.notification?.title,
+        body: payload?.notification?.body,
+        imageUrl,
+      },
+
+      android: {
+        priority: 'high',
+        ttl: 60 * 60 * 24,
+        notification: {
+          sound: payload?.notification?.sound,
+        },
+      },
+    };
+
+    try {
+      const result: BatchResponse = await firebaseAdmin.messaging().sendEachForMulticast(body, false);
+      return result;
+    } catch (error) {
+      /**
+       * Todo: Logger ,error
+       */
+      throw error;
+    }
   }
 }
