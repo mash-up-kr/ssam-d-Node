0a55f8ee231d4b3fbe337a496b4f97e0c6dcf3b1
marades <jkl9510357@gmail.com>
2023-08-06 23:22:57 +0900

    Test: 유저 탈퇴 api 테스트

--- test/mock/repositories/mock-room.repository.ts
+++ test/mock/repositories/mock-room.repository.ts
@@ -1,4 +1,8 @@
 import { RoomRepository } from 'src/repositories';
 import { MockRepository } from './mock.repository';
 
-export const MockRoomRepository = (): MockRepository<RoomRepository> => ({ get: jest.fn(), update: jest.fn() });
+export const MockRoomRepository = (): MockRepository<RoomRepository> => ({
+  get: jest.fn(),
+  update: jest.fn(),
+  getRoomIdsByUserId: jest.fn(),
+});
